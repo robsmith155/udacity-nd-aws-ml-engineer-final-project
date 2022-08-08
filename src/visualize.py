d8337194a85b5102acc542d7f8c151d0629af39c
Rob Smith <robsmith155@gmail.com>
2022-08-08 19:13:00 +0300

    notebook: add monai data augmentation pipeline overview
    This addition shows how to load and transform data using MONAI and comes
    up with a preliminary pipeline.

--- src/visualize.py
+++ src/visualize.py
@@ -4,9 +4,15 @@ import pathlib
 from typing import List, Union
 
 import matplotlib.pyplot as plt
+import monai
 import numpy as np
 import torch
-from monai.transforms import EnsureChannelFirstd, EnsureTyped, LoadImaged
+from monai.transforms import (
+    Compose,
+    EnsureChannelFirstd,
+    EnsureTyped,
+    LoadImaged,
+)
 from monai.visualize import blend_images
 from PIL import Image
 
@@ -184,3 +190,75 @@ def make_patient_data_gif(
         loop=0,
     )
     print(f"GIF file output to {output_path}.")
+
+
+def plot_monai_transformed_data(
+    data_dict: dict, transpose: bool = True, convert_uint8: bool = True
+) -> None:
+    """Plot of MRI image and segmentation mask output from MONAI transform
+
+    Args:
+        data_dict (dict): Dictionary containing the path to the image and mask data. Keys must be 'image' and 'mask'.
+        transpose (bool, optional): Whether the data should be transposed (i.e. rotated). Defaults to True.
+        convert_uint8 (bool, optional): Whether to convert the data to np.uint8. Defaults to True.
+    """
+    img = data_dict["image"].numpy()
+    mask = data_dict["mask"].numpy()
+
+    if convert_uint8:
+        img = img.astype(np.uint8)
+        mask = mask.astype(np.uint8)
+
+    if transpose:
+        img = np.transpose(img, (1, 2, 0))
+        mask = np.transpose(mask, (1, 2, 0))
+
+    plt.figure(figsize=(12, 6))
+    plt.subplot(1, 2, 1)
+    plt.imshow(img)
+    plt.colorbar()
+    plt.title("MRI")
+
+    plt.subplot(1, 2, 2)
+    plt.imshow(mask)
+    plt.colorbar()
+    plt.title("Mask")
+
+
+def plot_monai_pipeline_data(
+    data_dict: dict, transform_pipeline: monai.transforms.Compose
+) -> None:
+    """Plots input image and mask and four different augmented views after data passed through transformation pipeline.
+
+    Args:
+        data_dict (dict): Dictionary containing the path to the image and mask data. Keys must be 'image' and 'mask'.
+        transform_pipeline (monai.transforms.Compose): MONAI transformation pipeline.
+    """
+    raw_im = Image.open(data_dict["image"])
+    raw_im = np.array(raw_im)
+    raw_mask = Image.open(data_dict["mask"])
+    raw_mask = np.array(raw_mask)
+
+    fig, axes = plt.subplots(nrows=2, ncols=5, figsize=(20, 10))
+    col_names = ["Original", "Aug1", "Aug2", "Aug3", "Aug4"]
+
+    for ax, col in zip(axes[0], col_names):
+        ax.set_title(col)
+
+    axes[0, 0].imshow(raw_im)
+    axes[0, 0].axis("off")
+    axes[1, 0].imshow(raw_mask)
+    axes[1, 0].axis("off")
+
+    for i in range(4):
+        data_trans = transform_pipeline(data_dict)
+        img = data_trans["image"].numpy()
+        mask = data_trans["mask"].numpy()
+        img = np.transpose(img, (2, 1, 0))
+        mask = np.transpose(mask, (2, 1, 0))
+        axes[0, i + 1].imshow(img)
+        axes[0, i + 1].axis("off")
+        axes[1, i + 1].imshow(mask)
+        axes[1, i + 1].axis("off")
+
+    fig.tight_layout()
